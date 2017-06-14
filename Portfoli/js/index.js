e6412823bcab504f6e152c495f48fcd60cca9fc3
JoanRialp <joanrg98@gmail.com>
2017-06-14 16:47:17 +0200

    Add files via upload

---
+++ Portfoli/js/index.js
@@ -0,0 +1,116 @@
+PasE.style.display = 'block';
+PasE2.style.display = 'none';
+PasE3.style.display = 'none';
+PasE4.style.display = 'none';
+PasDInici.style.display = 'none';
+PasD.style.display = 'block';
+PasD2.style.display = 'none';
+PasD3.style.display = 'none';
+
+
+function PassarDretaInici()
+{
+ document.getElementById('Presentacio').src="Imatges/bazookaInici.jpg";
+    
+PasE.style.display = 'block';
+PasE2.style.display = 'none';
+PasE3.style.display = 'none';
+PasE4.style.display = 'none';
+PasDInici.style.display = 'none';
+PasD.style.display = 'block';
+PasD2.style.display = 'none';
+PasD3.style.display = 'none';
+}
+
+function PassarDreta()
+{
+ document.getElementById('Presentacio').src="Imatges/Lamp.jpg";
+    
+PasE.style.display = 'none';
+PasE2.style.display = 'block';
+PasDInici.style.display = 'none';
+PasD.style.display = 'none';
+PasD2.style.display = 'block';
+PasD3.style.display = 'none';
+}
+
+
+function PassarDreta2()
+{
+ document.getElementById('Presentacio').src="Imatges/Beisbol.jpg";
+    
+PasE.style.display = 'none';
+PasE2.style.display = 'none';
+PasE3.style.display = 'block';
+PasDInici.style.display = 'none';
+PasD.style.display = 'none';
+PasD2.style.display = 'none';
+PasD3.style.display = 'block';
+}
+
+function PassarDreta3()
+{
+ document.getElementById('Presentacio').src="Imatges/Linx.jpg";
+    
+PasE.style.display = 'none';
+PasE2.style.display = 'none';
+PasE3.style.display = 'none';
+PasE4.style.display = 'block';
+PasDInici.style.display = 'block';
+PasD.style.display = 'none';
+PasD2.style.display = 'none';
+PasD3.style.display = 'none';
+}
+
+function PassarEsquerra()
+{
+ document.getElementById('Presentacio').src="Imatges/Linx.jpg";
+    
+PasE.style.display = 'none';
+PasE2.style.display = 'none';
+PasE3.style.display = 'none';
+PasE4.style.display = 'block';
+PasDInici.style.display = 'block';
+PasD.style.display = 'none';
+PasD2.style.display = 'none';
+PasD3.style.display = 'none';
+}
+
+function PassarEsquerra2()
+{
+ document.getElementById('Presentacio').src="Imatges/bazookaInici.jpg";
+    
+PasE.style.display = 'block';
+PasE2.style.display = 'none';
+PasD.style.display = 'block';
+PasD2.style.display = 'none';
+PasD3.style.display = 'none';
+}
+
+function PassarEsquerra3()
+{
+ document.getElementById('Presentacio').src="Imatges/Lamp.jpg";
+    
+PasE.style.display = 'none';
+PasE2.style.display = 'block';
+PasE3.style.display = 'none';
+PasDInici.style.display = 'none';
+PasD.style.display = 'none';
+PasD2.style.display = 'block';
+PasD3.style.display = 'none';
+}
+
+function PassarEsquerra4()
+{
+ document.getElementById('Presentacio').src="Imatges/Beisbol.jpg";
+    
+PasE.style.display = 'none';
+PasE2.style.display = 'none';
+PasE3.style.display = 'block';
+PasE4.style.display = 'none';
+PasDInici.style.display = 'none';
+PasD.style.display = 'none';
+PasD2.style.display = 'none';
+PasD3.style.display = 'block';
+}
+
---
+++ Portfoli/js/media.js
@@ -0,0 +1,400 @@
+
+FotoAmpliada.style.display = 'none';
+FotoAmpliada2.style.display = 'none';
+FotoAmpliada3.style.display = 'none';
+FotoAmpliada4.style.display = 'none';
+ball.style.display = 'none';
+ball2.style.display = 'none';
+ball3.style.display = 'none';
+ball4.style.display = 'none';
+ballP.style.display = 'none';
+ballP2.style.display = 'none';
+ballP3.style.display = 'none';
+ballP4.style.display = 'none';
+ballE.style.display = 'none';
+ballE2.style.display = 'none';
+ballE3.style.display = 'none';
+creu.style.display = 'none';
+creu2.style.display = 'none';
+creu3.style.display = 'none';
+
+
+function Photoshop()
+{
+ document.getElementById('FotoAmpliada').src="Media/Photoshop/1.jpg";
+
+FotoAmpliada.style.display = 'block';
+FotoAmpliada2.style.display = 'none';
+ball.style.display = 'block';
+ball2.style.display = 'block';
+ball3.style.display = 'block';
+ball4.style.display = 'block';
+creu.style.display = 'block';
+    
+FotoAmpliada3.style.display = 'none';
+ballP.style.display = 'none';
+ballP2.style.display = 'none';
+ballP3.style.display = 'none';
+ballP4.style.display = 'none';
+creu2.style.display = 'none';
+    
+FotoAmpliada4.style.display = 'none';
+ballE.style.display = 'none';
+ballE2.style.display = 'none';
+ballE3.style.display = 'none';
+creu3.style.display = 'none';
+    
+}
+
+function Photo1()
+{
+    FotoAmpliada.style.display = 'block';
+FotoAmpliada2.style.display = 'none';
+ document.getElementById('FotoAmpliada').src="Media/Photoshop/1.jpg"; 
+
+}
+
+function Photo2()
+{
+FotoAmpliada.style.display = 'none';
+FotoAmpliada2.style.display = 'block';
+ document.getElementById('FotoAmpliada').src="Media/Photoshop/2.jpg"; 
+document.getElementById('FotoAmpliada2').src="Media/Photoshop/2.jpg"; 
+
+}
+
+function Photo3()
+{
+    FotoAmpliada.style.display = 'block';
+FotoAmpliada2.style.display = 'none';
+ document.getElementById('FotoAmpliada').src="Media/Photoshop/3.jpg"; 
+
+}
+
+function Photo4()
+{
+    FotoAmpliada.style.display = 'block';
+FotoAmpliada2.style.display = 'none';
+ document.getElementById('FotoAmpliada').src="Media/Photoshop/4.jpg"; 
+
+}
+
+function Photoshop2()
+{
+ document.getElementById('FotoAmpliada2').src="Media/Photoshop/2.jpg";
+
+FotoAmpliada.style.display = 'none';
+FotoAmpliada2.style.display = 'block';
+ball.style.display = 'block';
+ball2.style.display = 'block';
+ball3.style.display = 'block';
+ball4.style.display = 'block';
+creu.style.display = 'block';
+
+FotoAmpliada3.style.display = 'none';
+ballP.style.display = 'none';
+ballP2.style.display = 'none';
+ballP3.style.display = 'none';
+ballP4.style.display = 'none';
+creu2.style.display = 'none';
+    
+FotoAmpliada4.style.display = 'none';
+ballE.style.display = 'none';
+ballE2.style.display = 'none';
+ballE3.style.display = 'none';
+creu3.style.display = 'none';
+    
+}
+
+function Photoshop3()
+{
+ document.getElementById('FotoAmpliada').src="Media/Photoshop/3.jpg";
+
+FotoAmpliada.style.display = 'block';
+FotoAmpliada2.style.display = 'none';
+ball.style.display = 'block';
+ball2.style.display = 'block';
+ball3.style.display = 'block';
+ball4.style.display = 'block';
+creu.style.display = 'block';
+    
+FotoAmpliada3.style.display = 'none';
+ballP.style.display = 'none';
+ballP2.style.display = 'none';
+ballP3.style.display = 'none';
+ballP4.style.display = 'none';
+creu2.style.display = 'none';
+    
+FotoAmpliada4.style.display = 'none';
+ballE.style.display = 'none';
+ballE2.style.display = 'none';
+ballE3.style.display = 'none';
+creu3.style.display = 'none';
+    
+}
+
+function Photoshop4()
+{
+ document.getElementById('FotoAmpliada').src="Media/Photoshop/4.jpg";
+
+FotoAmpliada.style.display = 'block';
+FotoAmpliada2.style.display = 'none';
+ball.style.display = 'block';
+ball2.style.display = 'block';
+ball3.style.display = 'block';
+ball4.style.display = 'block';
+creu.style.display = 'block';
+    
+FotoAmpliada3.style.display = 'none';
+ballP.style.display = 'none';
+ballP2.style.display = 'none';
+ballP3.style.display = 'none';
+ballP4.style.display = 'none';
+creu2.style.display = 'none';
+    
+FotoAmpliada4.style.display = 'none';
+ballE.style.display = 'none';
+ballE2.style.display = 'none';
+ballE3.style.display = 'none';
+creu3.style.display = 'none';
+    
+}
+
+function Illuminacio()
+{
+ document.getElementById('FotoAmpliada3').src="Media/Illuminacio/1.jpg";
+
+FotoAmpliada3.style.display = 'block';
+ballP.style.display = 'block';
+ballP2.style.display = 'block';
+ballP3.style.display = 'block';
+ballP4.style.display = 'block';
+creu2.style.display = 'block';
+
+FotoAmpliada.style.display = 'none';
+FotoAmpliada2.style.display = 'none';
+ball.style.display = 'none';
+ball2.style.display = 'none';
+ball3.style.display = 'none';
+ball4.style.display = 'none';
+creu.style.display = 'none';
+    
+FotoAmpliada4.style.display = 'none';
+ballE.style.display = 'none';
+ballE2.style.display = 'none';
+ballE3.style.display = 'none';
+creu3.style.display = 'none';
+    
+}
+
+function Illuminacio2()
+{
+ document.getElementById('FotoAmpliada3').src="Media/Illuminacio/2.jpg";
+
+FotoAmpliada3.style.display = 'block';
+ballP.style.display = 'block';
+ballP2.style.display = 'block';
+ballP3.style.display = 'block';
+ballP4.style.display = 'block';
+creu2.style.display = 'block';
+
+FotoAmpliada.style.display = 'none';
+FotoAmpliada2.style.display = 'none';
+ball.style.display = 'none';
+ball2.style.display = 'none';
+ball3.style.display = 'none';
+ball4.style.display = 'none';
+creu.style.display = 'none';
+
+FotoAmpliada4.style.display = 'none';
+ballE.style.display = 'none';
+ballE2.style.display = 'none';
+ballE3.style.display = 'none';
+creu3.style.display = 'none';
+    
+}
+
+function Illuminacio3()
+{
+ document.getElementById('FotoAmpliada3').src="Media/Illuminacio/3.jpg";
+
+FotoAmpliada3.style.display = 'block';
+ballP.style.display = 'block';
+ballP2.style.display = 'block';
+ballP3.style.display = 'block';
+ballP4.style.display = 'block';
+creu2.style.display = 'block';
+
+FotoAmpliada.style.display = 'none';
+FotoAmpliada2.style.display = 'none';
+ball.style.display = 'none';
+ball2.style.display = 'none';
+ball3.style.display = 'none';
+ball4.style.display = 'none';
+creu.style.display = 'none';
+    
+FotoAmpliada4.style.display = 'none';
+ballE.style.display = 'none';
+ballE2.style.display = 'none';
+ballE3.style.display = 'none';
+creu3.style.display = 'none';
+    
+}
+
+function Illuminacio4()
+{
+ document.getElementById('FotoAmpliada3').src="Media/Illuminacio/4.jpg";
+
+FotoAmpliada3.style.display = 'block';
+ballP.style.display = 'block';
+ballP2.style.display = 'block';
+ballP3.style.display = 'block';
+ballP4.style.display = 'block';
+creu2.style.display = 'block';
+
+FotoAmpliada.style.display = 'none';
+FotoAmpliada2.style.display = 'none';
+ball.style.display = 'none';
+ball2.style.display = 'none';
+ball3.style.display = 'none';
+ball4.style.display = 'none';
+creu.style.display = 'none';
+
+FotoAmpliada4.style.display = 'none';
+ballE.style.display = 'none';
+ballE2.style.display = 'none';
+ballE3.style.display = 'none';
+creu3.style.display = 'none';
+    
+}
+
+function Illum1()
+{
+    FotoAmpliada3.style.display = 'block';
+ document.getElementById('FotoAmpliada3').src="Media/Illuminacio/1.jpg"; 
+
+}
+
+function Illum2()
+{
+    FotoAmpliada3.style.display = 'block';
+ document.getElementById('FotoAmpliada3').src="Media/Illuminacio/2.jpg"; 
+
+}
+
+function Illum3()
+{
+    FotoAmpliada3.style.display = 'block';
+ document.getElementById('FotoAmpliada3').src="Media/Illuminacio/3.jpg"; 
+
+}
+
+function Illum4()
+{
+    FotoAmpliada3.style.display = 'block';
+ document.getElementById('FotoAmpliada3').src="Media/Illuminacio/4.jpg"; 
+
+}
+
+function Escultura()
+{
+ document.getElementById('FotoAmpliada4').src="Media/Escultura/1.jpg";
+
+FotoAmpliada4.style.display = 'block';
+ballE.style.display = 'block';
+ballE2.style.display = 'block';
+ballE3.style.display = 'block';
+creu3.style.display = 'block';
+    
+FotoAmpliada3.style.display = 'none';
+ballP.style.display = 'none';
+ballP2.style.display = 'none';
+ballP3.style.display = 'none';
+ballP4.style.display = 'none';
+creu2.style.display = 'none';
+    
+FotoAmpliada.style.display = 'none';
+FotoAmpliada2.style.display = 'none';
+ball.style.display = 'none';
+ball2.style.display = 'none';
+ball3.style.display = 'none';
+ball4.style.display = 'none';
+creu.style.display = 'none';
+    
+}
+
+function Escultura2()
+{
+ document.getElementById('FotoAmpliada4').src="Media/Escultura/3.jpg";
+
+FotoAmpliada4.style.display = 'block';
+ballE.style.display = 'block';
+ballE2.style.display = 'block';
+ballE3.style.display = 'block';
+creu3.style.display = 'block';
+    
+FotoAmpliada3.style.display = 'none';
+ballP.style.display = 'none';
+ballP2.style.display = 'none';
+ballP3.style.display = 'none';
+ballP4.style.display = 'none';
+creu2.style.display = 'none';
+    
+FotoAmpliada.style.display = 'none';
+FotoAmpliada2.style.display = 'none';
+ball.style.display = 'none';
+ball2.style.display = 'none';
+ball3.style.display = 'none';
+ball4.style.display = 'none';
+creu.style.display = 'none';
+    
+}
+
+function Escultura3()
+{
+ document.getElementById('FotoAmpliada4').src="Media/Escultura/2.jpg";
+
+FotoAmpliada4.style.display = 'block';
+ballE.style.display = 'block';
+ballE2.style.display = 'block';
+ballE3.style.display = 'block';
+creu3.style.display = 'block';
+    
+FotoAmpliada3.style.display = 'none';
+ballP.style.display = 'none';
+ballP2.style.display = 'none';
+ballP3.style.display = 'none';
+ballP4.style.display = 'none';
+creu2.style.display = 'none';
+    
+FotoAmpliada.style.display = 'none';
+FotoAmpliada2.style.display = 'none';
+ball.style.display = 'none';
+ball2.style.display = 'none';
+ball3.style.display = 'none';
+ball4.style.display = 'none';
+creu.style.display = 'none';
+    
+}
+
+
+function Escul1()
+{
+    FotoAmpliada3.style.display = 'block';
+ document.getElementById('FotoAmpliada4').src="Media/Escultura/1.jpg"; 
+
+}
+
+function Escul2()
+{
+    FotoAmpliada3.style.display = 'block';
+ document.getElementById('FotoAmpliada4').src="Media/Escultura/3.jpg"; 
+
+}
+
+function Escul3()
+{
+    FotoAmpliada3.style.display = 'block';
+ document.getElementById('FotoAmpliada4').src="Media/Escultura/2.jpg"; 
+
+}
